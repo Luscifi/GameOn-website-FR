46681b8a9af5fb0dcf6d789da73c9627439f080b
Lucie Vasseur <vasseur.lucie@gmail.com>
2023-06-16 13:51:34 +0200

    Fermeture du formaulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelectorAll(".close");
+const btn-submit = document.querySelectorAll(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,22 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+close.forEach((span) => span.addEventListener("click", closeModal));
+
+// close modal form 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// moal form submit
+.forEach((btn) => span.addEventListener("click", closeModal));
+
+// form success
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
